0197a8902136315ac3025a4c7e53243171438956
Michael Nutt <michael@nuttnet.net>
2018-03-06 22:25:12 -0500

    switch to line reporter

--- index.js
+++ index.js
@@ -23,17 +23,22 @@ FailureReporter.prototype = {
   },
 
   display: function(prefix, result) {
+    var out = this.out;
+    function print(msg) {
+      out.write(msg + '\n');
+    }
+
+    var name = result.name.trim().replace(/^Exam Partition /, '');
+
     if (result.skipped) {
-      this.out.write(colors.yellow('*'));
-      return;
+      return print(colors.yellow('* ') + name);
     }
 
     if (result.passed) {
-      this.out.write(colors.green('.'));
-      return;
+      return print(colors.green('\u2713\uFE0F ') + name);
     }
 
-    this.out.write('\n' + colors.red('F ' + result.name.trim())+': ');
+    print(colors.red('F ' + name));
 
     if(result.error && result.error.message) {
       // result.error.message is the whole stack trace
@@ -42,12 +47,12 @@ FailureReporter.prototype = {
       // at http://localhost:7357/assets/test-support.js:5463: No model was found for 'user'
       // line number and file aren't useful because it's the concatenated file
       if (last.length > 1) {
-        this.out.write(last[1]+'\n');
+        print(last[1]);
       } else {
-        this.out.write(lines[lines.length-1]+'\n');
+        print(lines[lines.length-1]);
       }
 
-      this.out.write(result.error.message.replace(/\n/g, '\n    ') + '\n');
+      print(result.error.message.replace(/\n/g, '\n    '));
     }
   },
 
@@ -56,13 +61,15 @@ FailureReporter.prototype = {
       return;
     }
 
-    var summary = this.fail+'/'+this.total+' failed\n';
+    var summary = this.fail+'/'+this.total+' failed ';
+    var thumbsup = '\uD83D\uDC4D';
+    var thumbsdown = '\uD83D\uDC4E';
 
     this.out.write('\n');
     if (this.fail === 0) {
-      this.out.write(colors.green(summary));
+      this.out.write(colors.green(summary) + thumbsup + '\n');
     } else {
-      this.out.write(colors.red(summary));
+      this.out.write(colors.red(summary) + thumbsdown + '\n');
     }
   }
 };
